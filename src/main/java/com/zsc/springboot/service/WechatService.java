05f2c4ce5f06c1b09375aa1ff179d2a95fd652be
zhangshichen <shichen@17shihui.com>
2018-12-07 16:36:19 +0800

    增加对apollojsonvalue注解的使用

--- src/main/java/com/zsc/springboot/service/WechatService.java
+++ src/main/java/com/zsc/springboot/service/WechatService.java
@@ -3,6 +3,8 @@ package com.zsc.springboot.service;
 import com.ctrip.framework.apollo.model.ConfigChange;
 import com.ctrip.framework.apollo.model.ConfigChangeEvent;
 import com.ctrip.framework.apollo.spring.annotation.ApolloConfigChangeListener;
+import com.ctrip.framework.apollo.spring.annotation.ApolloJsonValue;
+import com.zsc.springboot.service.wechat.WechatConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -29,12 +31,15 @@ public class WechatService {
     @Value("${wechat.path:http://127.0.0.1:8181}")
     private String wechatPath;
 
+    @ApolloJsonValue("${wechat.config}")
+    private WechatConfig wechatConfig;
+
     /**
      * 配置变化监听器
      *
      * @param configChangeEvent
      */
-    @ApolloConfigChangeListener(interestedKeys = {"wechat.appid", "wechat.secret", "wechat.path"})
+    @ApolloConfigChangeListener(interestedKeys = {"wechat.appid", "wechat.secret", "wechat.path", "wechat.config"})
     public void wechatOnChangeListener(ConfigChangeEvent configChangeEvent) {
         Set<String> changeKeys = configChangeEvent.changedKeys();
         changeKeys.stream().forEach(key -> {
---
+++ src/main/java/com/zsc/springboot/service/wechat/WechatConfig.java
@@ -0,0 +1,18 @@
+package com.zsc.springboot.service.wechat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author shichen
+ * @create 2018-12-07
+ * @desc
+ */
+@Getter
+@Setter
+@ToString
+public class WechatConfig {
+    private String appid;
+    private String secret;
+}
