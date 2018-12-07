95bfaf5d05414dfdfbd871cbd66d8f1b2d6edc06
zhangshichen <shichen@17shihui.com>
2018-12-07 18:53:23 +0800

    增加与xxl-job结合例子

---
+++ src/main/java/com/zsc/springboot/job/DemoJobHandler.java
@@ -0,0 +1,27 @@
+package com.zsc.springboot.job;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author shichen
+ * @create 2018-12-07
+ * @desc
+ */
+@Slf4j
+@JobHandler(value="demoJobHandler")
+@Component
+public class DemoJobHandler extends IJobHandler {
+
+    @Override
+    public ReturnT<String> execute(String param) throws Exception {
+        log.info("【定时任务执行】param {}, current time {}", param, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        return ReturnT.SUCCESS;
+    }
+}
---
+++ src/main/java/com/zsc/springboot/job/config/XxlJobConfig.java
@@ -0,0 +1,57 @@
+package com.zsc.springboot.job.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author shichen
+ * @create 2018-12-07
+ * @desc
+ */
+@Slf4j
+@Configuration
+@PropertySource(value = "classpath:application-job.properties")
+public class XxlJobConfig {
+
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.executor.appname}")
+    private String appName;
+
+    @Value("${xxl.job.executor.ip}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port}")
+    private int port;
+
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.logpath}")
+    private String logPath;
+
+    @Value("${xxl.job.executor.logretentiondays}")
+    private int logRetentionDays;
+
+
+    @Bean(initMethod = "start", destroyMethod = "destroy")
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        log.info("【定时任务】xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppName(appName);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        return xxlJobSpringExecutor;
+    }
+
+}
