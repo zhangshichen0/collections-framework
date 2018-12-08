4e29386d38e8fa177bf79fba08b60c3b58fc2386
zhangshichen <shichen@17shihui.com>
2018-12-08 22:17:06 +0800

    修改Bootstrap配置

--- src/main/java/com/zsc/springboot/Bootstrap.java
+++ src/main/java/com/zsc/springboot/Bootstrap.java
@@ -3,6 +3,15 @@ package com.zsc.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @author shichen
@@ -11,10 +20,42 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  */
 @SpringBootApplication(scanBasePackages = "com.zsc.springboot")
 @EnableConfigurationProperties
-public class Bootstrap {
+@EnableWebMvc
+@EnableAspectJAutoProxy
+@EnableScheduling
+public class Bootstrap extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class, args);
     }
 
+    /**
+     * 增加消息转换器
+     *
+     * @param converters
+     */
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+    }
+
+    /**
+     * 增加拦截器
+     *
+     * @param registry
+     */
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+    }
+
+    /**
+     * 加载资源文件
+     *
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+    }
 }
